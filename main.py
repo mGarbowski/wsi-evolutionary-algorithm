21c759756d8a6c17c6240cb3bd81e49f98447ff9
mgarbowski <mikolaj.garbowski@gmail.com>
2023-10-29 12:39:30 +0100

    implement tournament reproduction

--- main.py
+++ main.py
@@ -13,7 +13,17 @@ def find_best(population: np.ndarray, fitness: np.ndarray) -> tuple[np.ndarray, 
 
 
 def tournament_reproduction(population: np.ndarray, fitness: np.ndarray) -> np.ndarray:
-    return population  # TODO
+    next_population = np.empty(shape=population.shape)
+    population_size = population.shape[0]
+
+    for idx in range(population_size):
+        i, j = np.random.choice(a=population_size, size=2, replace=False)
+        if fitness[i] <= fitness[j]:
+            next_population[idx] = population[i]
+        else:
+            next_population[idx] = population[j]
+
+    return next_population
 
 
 def mutate(population: np.ndarray, mutation_strength: float):
@@ -67,6 +77,7 @@ def main():
 
     minimum = evolution(f13)
     print(minimum)
+
 
 if __name__ == '__main__':
     main()
